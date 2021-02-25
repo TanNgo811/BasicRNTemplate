e5a334115dd245f080a9942bb7f0d0aedbfcb9ee
Ton Corn Go <ngotientan811@gmail.com>
2021-02-25 07:43:40 +0700

    To do Item Segment + Carousel

---
+++ src/unsplashAPI/UnsplashAPI.tsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+import axios from 'axios';
+import {Image, View} from 'react-native';
+
+export default class UnsplashAPI extends Component<any, any> {
+  render() {
+    return <View />;
+  }
+}
